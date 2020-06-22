2c35527888adbd274d4bfb03a24b368b8b865a78
fazal <fazi@tests-MacBook-Pro.local>
2020-06-22 13:43:12 +0100

    updated project plan

--- Code/src/main/scala/Example.scala
+++ Code/src/main/scala/Example.scala
@@ -18,16 +18,16 @@ import ujson.{Obj, Value}
 object Example extends App {
 
   // some dummy data
-  val col1 = Col(List("a", "b", "c", "d", "e"))
-  val col2 = Col(List(12, 200, 80900, 201200, 124420000))
-  val col3 = Col(List(121, 12121, 71240000, 44356, 845))
-
-  val header: Col[String] = Col(List("a1", "b1", "c1"))
-  val data = List(col1, col2, col3)
-
-  val table = Table(header, data)
-  table.plot()
-
+//  val col1 = Col(List("a", "b", "c", "d", "e"))
+//  val col2 = Col(List(12, 200, 80900, 201200, 124420000))
+//  val col3 = Col(List(121, 12121, 71240000, 44356, 845))
+//
+//  val header: Col[String] = Col(List("a1", "b1", "c1"))
+//  val data = List(col1, col2, col3)
+//
+//  val table = Table(header, data)
+//  table.plot()
+  
 }
 
 

8cb471ead7561e1eeff71ec35e731e3bb469343a
fazi <fazi@fazis-MacBook-Pro.local>
2020-07-15 21:25:40 +0100

    need to fix type conversion

--- Code/src/main/scala/org/carbonateresearch/picta/options/Grid.scala
+++ Code/src/main/scala/org/carbonateresearch/picta/options/Grid.scala
@@ -10,5 +10,12 @@ import ujson.{Obj, Value}
  * @param pattern : Specifies the pattern for the grid.
  */
 final case class Subplot(rows: Int = 1, columns: Int = 1, pattern: String = "independent") extends Component {
+
+  val grid: Array[Array[(XAxis, YAxis)]] = Array.ofDim[(XAxis, YAxis)](rows, columns)
+
+  def setAxis(row: Int, col: Int, xaxis: XAxis, yaxis: YAxis): Unit = this.grid(row)(col) = (xaxis, yaxis)
+
+  
+
   private[picta] def serialize(): Value = Obj("rows" -> rows, "columns" -> columns, "pattern" -> pattern)
 }
